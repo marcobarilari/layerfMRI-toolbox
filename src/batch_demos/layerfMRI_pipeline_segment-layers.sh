1e02ea32efbf87039e03a6d4d65d3c6764a6390b
marcobarilari <marcobarilr@gmail.com>
2024-02-13 16:46:10 -0500

    edit scripts to run suma upsampling

--- src/batch_demos/layerfMRI_pipeline_segment-layers.sh
+++ src/batch_demos/layerfMRI_pipeline_segment-layers.sh
@@ -6,7 +6,7 @@ raw_dir=${root_dir}/inputs/raw
 derivatives_dir=${root_dir}/derivatives_layerfMRI-toolbox
 code_dir=${root_dir}/code
 
-vasopipeline_logfiles_dir=${derivatives_dir}/vasopipeline-logfiles
+vasopipeline_logfiles_dir=${derivatives_dir}/layerfMRI-logfiles
 freesurfer_reconall_dir=${derivatives_dir}/freesurfer-reconall
 vasopipeline_mesh_dir=${derivatives_dir}/vasopipeline-surface-mesh
 vasopipeline_layers_dir=${derivatives_dir}/vasopipeline-layers
@@ -22,7 +22,9 @@ sesID="02"
 
 ## Run presurfer
 
-## Run freesurfer
+WIP
+
+## Run freesurfer recon-all
 
 run_freesurfer_recon_all.sh
 
@@ -30,9 +32,11 @@ run_freesurfer_recon_all.sh
 
 fs_surf_path=${freesurfer_reconall_dir}/surf #sub
 
-run_suma_fs_to_surface.sh $fs_surf_path $subID
+run_suma_fs_to_surface.sh \
+    $fs_surf_path \
+    $subID
 
-## Resamle anatomical
+## Resample anatomical
 
 image_to_resample=
 output_dir=
@@ -45,14 +49,17 @@ resample_image_iso.sh \
     $output_filename \
     $resample_factor_iso
 
-## Run surf to vol
+## Upsample the surface image
 
 # Number of edge divides for linear icosahedron tesselation 
 # the higehr the number the long the computation is
 
 SUMA_dir=${fs_surf_path}
-upsampled_anat=
 
+upsampled_anat=${output_dir}/${$output_filename}
+
+# Number of edge divides for linear icosahedron tesselation 
+# the higehr the number the long the computation is
 linDepth=2000
 
 hemisphere="lh"
@@ -72,3 +79,34 @@ upsample_SUMA_surface.sh \
     $subID \
     $linDepth \
     $hemisphere
+
+## map_surface_to_volume_GM_WM
+
+map_surface_to_volume_GM_WM.sh <SUMA_dir> <upsampled_anat> <subID> <linDepth> <hemisphere>
+
+map_surface_to_volume_GM_WM.sh <SUMA_dir> <upsampled_anat> <subID> <linDepth> <hemisphere>
+
+## Make the RIM (rim012.nii.gz) to be inspected and manually edited if necessary
+
+combing_GM_WM.sh
+
+## Bring the RIM to the EPI space
+
+coregister_anat_to_epi.sh
+
+coregister_mask_to_epi.sh
+
+################################################################
+#      VISUAL INSPECTION AND MANUAL EDITING IF NECESSARY       #
+################################################################
+
+## Make final RIM with 1 2 3 labels 
+
+make_rim.sh
+
+## Make layers
+
+make_layers.sh
+
+
+
