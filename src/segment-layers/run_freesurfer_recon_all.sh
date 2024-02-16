3c1e8272ca3586f372bcd0cb9b059a3e6cf226a0
marcobarilari <marcobarilr@gmail.com>
2024-02-15 20:37:09 -0500

    add recon all wrapper debugged

--- src/segment-layers/run_freesurfer_recon_all.sh
+++ src/segment-layers/run_freesurfer_recon_all.sh
@@ -1,6 +1,6 @@
 # !bin/bash
 
-# set -e
+set -e
 
 ## Run freesurfer recon-all for high-res data
 #
@@ -8,35 +8,29 @@
 
 # no need to print out the recon-all logfile since freesurfer takes care of it
 # you can find it in <freesurfer-output>/scripts/recon-all.log
-# print_process_logfile.sh 'recon-all'
-
-source config_layerfMRI_pipeline.sh
 
 filename=$1
 output_dir=$2
 openmp=$3
 
-echo $filename
-echo $output_dir
-echo $openmp
+echo ""
+echo "Will run recon-all on: "
+echo " - $filename"
 
-
-rm -rf $output_dir
+# remove the recon-all output otherwise freesurfer complains
+rm -rf $output_dir/fresurfer
 mkdir -p $output_dir
 
-echo $layerfmri_toolbox_dir
+SUBJECTS_DIR=$output_dir
 
-# recon-all -all \
-#     -s $output_dir \
-#     -hires \
-#     -i $filename \
-#     -expert expert.opts \ # add path?
-#     -parallel -openmp $openmp
+recon-all -all \
+    -s $output_dir/fresurfer \
+    -hires \
+    -i $filename \
+    -expert expert.opts \ # add path?
+    -parallel -openmp $openmp
 
-
-# recon-all -all \
-#     -s /mnt/HD_jupiter/marcobarilari/sandbox/sandbox_layerfMRI-pipeline/outputs/derivatives/layerfMRI-segmentation/sub-SC08/freesurfer \
-#     -hires \
-#     -i /mnt/HD_jupiter/marcobarilari/sandbox/sandbox_layerfMRI-pipeline/outputs/derivatives/layerfMRI-segmentation/sub-SC08/presurf_MPRAGEise/presurf_biascorrect/sub-SC08_ses-02_acq-r0p75_UNIT1_MPRAGEised_biascorrected.nii \
-#     -expert $layerfmri_toolbox_dir/src/segment-layers/expert.opts \
-#     -parallel -openmp 4
+echo ""
+echo "recon-all done!"
+echo "Find the output here : $output_dir"
+echo ""
