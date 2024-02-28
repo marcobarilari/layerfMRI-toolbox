51bd3b223b36f6e0ae282f4bd30f116c2684ff3e
mrneont <neon.taylor@gmail.com>
2024-02-28 17:44:02 -0500

    convert_afni_surface_to_volume_tissue_mask.sh: spacing/formatting

--- src/segment-layers/convert_afni_surface_to_volume_tissue_mask.sh
+++ src/segment-layers/convert_afni_surface_to_volume_tissue_mask.sh
@@ -4,12 +4,13 @@ set -e
 
 ## Map back surface to volume and work on GM WM masks
 #
-# usage: convert_surface_to_volume_tissue_mask.sh <SUMA_dir> <upsampled_anat> <subID> <linDepth> <hemisphere>
+# usage: convert_surface_to_volume_tissue_mask.sh <SUMA_dir> 
+#                <upsampled_anat> <subID> <linDepth> <hemisphere>
 #
 # Adapted from https://github.com/kenshukoiso/Whole_Brain_Project/blob/main/script/s06_thinner_pial.sh
 
-print_process_logfile.sh \
-    $vasopipeline_logfiles_dir \
+print_process_logfile.sh                                                     \
+    $vasopipeline_logfiles_dir                                               \
     $(basename "$0" .sh)
 
 suma_dir=$1
@@ -34,59 +35,59 @@ echo ""
 echo "++ Generate RIBBON volume mask"
 echo ""
 
-3dSurf2Vol \
-    -spec std_BOTH.ld${linDepth}.${hemisphere}.spec \
-    -surf_A std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii \
-    -surf_B std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii \
-    -sv T1.nii \
-    -gridset $upsampled_anat \
-    -map_func mask \
-    -f_steps 40 \
-    -f_index points \
-    -f_p1_fr 1.05 \
-    -f_pn_fr 0.1 \
-    -prefix $output_dir/ribbonmask_${linDepth}_${hemisphere}_min_1p05_0p1.nii.gz \
+3dSurf2Vol                                                                   \
+    -spec      std_BOTH.ld${linDepth}.${hemisphere}.spec                     \
+    -surf_A    std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii    \
+    -surf_B    std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii        \
+    -sv        T1.nii                                                        \
+    -gridset   $upsampled_anat                                               \
+    -map_func  mask                                                          \
+    -f_steps   40                                                            \
+    -f_index   points                                                        \
+    -f_p1_fr   1.05                                                          \
+    -f_pn_fr   0.1                                                           \
+    -prefix    $output_dir/ribbonmask_${linDepth}_${hemisphere}_min_1p05_0p1.nii.gz \
     -overwrite
 
 echo "Generate CSF volume mask"
 
-3dSurf2Vol \
-    -spec std_BOTH.ld${linDepth}.${hemisphere}.spec \
-    -surf_A std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii \
-    -surf_B std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii \
-    -sv T1.nii \
-    -gridset $upsampled_anat \
-    -map_func mask \
-    -f_steps 40 \
-    -f_index points \
-    -f_p1_fr 0.90 \
-    -f_pn_fr 0.0 \
-    -prefix $output_dir/supra6_pial_${linDepth}_${hemisphere}.nii.gz \
+3dSurf2Vol                                                                   \
+    -spec      std_BOTH.ld${linDepth}.${hemisphere}.spec                     \
+    -surf_A    std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii    \
+    -surf_B    std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii        \
+    -sv        T1.nii                                                        \
+    -gridset   $upsampled_anat                                               \
+    -map_func  mask                                                          \
+    -f_steps   40                                                            \
+    -f_index   points                                                        \
+    -f_p1_fr   0.90                                                          \
+    -f_pn_fr   0.0                                                           \
+    -prefix    $output_dir/supra6_pial_${linDepth}_${hemisphere}.nii.gz      \
     -overwrite
 
-3dcalc \
-    -a $output_dir/ribbonmask_${linDepth}_${hemisphere}_min_1p05_0p1.nii.gz \
-    -b $output_dir/supra6_pial_${linDepth}_${hemisphere}.nii.gz \
-    -expr 'a*b' \
-    -prefix $output_dir/supra7_pial_${linDepth}_${hemisphere}.nii.gz
+3dcalc                                                                       \
+    -a       $output_dir/ribbonmask_${linDepth}_${hemisphere}_min_1p05_0p1.nii.gz \
+    -b       $output_dir/supra6_pial_${linDepth}_${hemisphere}.nii.gz        \
+    -expr    'a*b'                                                           \
+    -prefix  $output_dir/supra7_pial_${linDepth}_${hemisphere}.nii.gz
 
 # generate GM
 
 echo "++ Generate GM volume mask"
 
 # to test w/o applying any distance calculation #####
-3dSurf2Vol \
-    -spec std_BOTH.ld${linDepth}.${hemisphere}.spec \
-    -surf_A std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii \
-    -surf_B std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii \
-    -sv T1.nii \
-    -gridset $upsampled_anat \
-    -map_func mask \
-    -f_steps 40 \
-    -f_index points \
-    -f_p1_fr 0.0 \
-    -f_pn_fr 0.0 \
-    -prefix $output_dir/GM_${hemisphere}.nii.gz \
+3dSurf2Vol                                                                   \
+    -spec      std_BOTH.ld${linDepth}.${hemisphere}.spec                     \
+    -surf_A    std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii    \
+    -surf_B    std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii        \
+    -sv        T1.nii                                                        \
+    -gridset   $upsampled_anat                                               \
+    -map_func  mask                                                          \
+    -f_steps   40                                                            \
+    -f_index   points                                                        \
+    -f_p1_fr   0.0                                                           \
+    -f_pn_fr   0.0                                                           \
+    -prefix    $output_dir/GM_${hemisphere}.nii.gz                           \
     -overwrite
 #####################################################
 
@@ -95,18 +96,18 @@ echo "++ Generate WM volume mask"
 # remove pial by subtracting 7 supra7_pial_${linDepth}_${hemisphere}
 # to have GM without kissing gyri
 # generate WM
-3dSurf2Vol \
-    -spec std_BOTH.ld${linDepth}.${hemisphere}.spec \
-    -surf_A std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii \
-    -surf_B std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii \
-    -sv T1.nii \
-    -gridset $upsampled_anat \
-    -map_func mask \
-    -f_steps 40 \
-    -f_index points \
-    -f_p1_fr -1.0 \
-    -f_pn_fr 0.0 \
-    -prefix $output_dir/WM_${hemisphere}.nii.gz \
+3dSurf2Vol                                                                   \
+    -spec      std_BOTH.ld${linDepth}.${hemisphere}.spec                     \
+    -surf_A    std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii    \
+    -surf_B    std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii        \
+    -sv        T1.nii                                                        \
+    -gridset   $upsampled_anat                                               \
+    -map_func  mask                                                          \
+    -f_steps   40                                                            \
+    -f_index   points                                                        \
+    -f_p1_fr   -1.0                                                          \
+    -f_pn_fr   0.0                                                           \
+    -prefix    $output_dir/WM_${hemisphere}.nii.gz                           \
     -overwrite
 
 # Go back to previous working directory
