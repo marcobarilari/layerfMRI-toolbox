d532aed49bd61e2b6ba3d2443ad4364a1e0a32b5
mrneont <neon.taylor@gmail.com>
2024-02-28 14:47:52 -0500

    batch_demos script: some comments to echos

--- batch_demos/layerfMRI_pipeline_segment-layers.sh
+++ batch_demos/layerfMRI_pipeline_segment-layers.sh
@@ -35,7 +35,8 @@ source ${code_dir}/lib/layerfMRI-toolbox/config_layerfMRI_pipeline.sh
 
 mem_cpu_logger.sh start marcobarilari
 
-## Get raw data (bidslike files)
+echo "++ Get raw data (bidslike files)"
+
 import_raw_bidslike.sh                                                        \
     $raw_dir                                                                  \
     $lfmri_fs_seg_dir                                                         \
@@ -43,7 +44,7 @@ import_raw_bidslike.sh                                                        \
     $sesID                                                                    \
     $modality
 
-## Remove the MP2RAGE noise via presurfer
+++ "Remove the MP2RAGE noise via presurfer"
 
 anat_dir=$lfmri_fs_seg_dir/${subj}/anat
 UNIT1_image=${anat_dir}/${subj}_${ses}_acq-r0p75_UNIT1.nii
@@ -56,7 +57,7 @@ $matlabpath -nodisplay -nosplash -nodesktop                                   \
     run_presurfer_denoise(UNIT1, INV2);                                       \
     exit"
 
-## Run SPM12 bias field correction via presurfer
+echo "++ Run SPM12 bias field correction via presurfer"
 
 UNIT1_image=${anat_dir}/presurf_MPRAGEise/${subj}_${ses}_acq-r0p75_UNIT1_MPRAGEised.nii
 
@@ -66,23 +67,22 @@ $matlabpath -nodisplay -nosplash -nodesktop                                   \
     run_presurfer_biasfieldcorr(UNIT1); \
     exit"
 
-# ## Run freesurfer recon-all 
-# #  ***this will take at least 5h on crunch machines***
+echo "++ Run freesurfer recon-all"
+# ***this will take at least 5h on crunch machines***
+# NB: in $output_dir, freesurfer will create a folder called
+# `freesurfer/freesurfer`. 
+# I don't know what is the best naming option atm
 
 anat_image=${anat_dir}/presurf_MPRAGEise/presurf_biascorrect/${subj}_${ses}_acq-r0p75_UNIT1_MPRAGEised_biascorrected.nii
 output_dir=${anat_dir}
 openmp=4
 
-# NB: in $output_dir, freesurfer will create a folder called
-# `freesurfer/freesurfer`. 
-# I don't know what is the best naming option atm
-
 run_freesurfer_recon_all.sh                                                   \
     $anat_image                                                               \
-    $lfmri_fs_seg_dir/${subj}/freesurfer                                 \
+    $lfmri_fs_seg_dir/${subj}/freesurfer                                      \
     $openmp
 
-## Run suma reconstruction 
+echo "++ Run suma reconstruction"
 
 fs_surf_path=$lfmri_fs_seg_dir/${subj}/freesurfer/freesurfer/surf
 suma_output_dir=$lfmri_mesh_dir/${subj}
@@ -92,9 +92,9 @@ run_suma_fs_to_surface.sh                                                     \
     $suma_output_dir                                                          \
     ${subj}
 
-## Resample anatomical
-#  The image will used as a reference for the resampling of the surface 
-#  images
+echo "++ Resample anatomical"
+# The image will used as a reference for the resampling of the surface 
+# images
 
 image_to_resample=$suma_output_dir/SUMA/T1.nii.gz
 output_dir=$suma_output_dir
@@ -107,7 +107,8 @@ resample_afni_image_iso_factor.sh                                             \
     $output_filename                                                          \
     $resample_iso_factor
 
-## Upsample the surface image 
+echo "++ Upsample the surface image"
+
 #  *** with `linDepth=2000` this will take long time (up to 4h) and a hog 
 #      memory (up 40 GB) *per hemisphere*
 #  *** to make the process faster you can run the two hemispheres in parallel
@@ -140,7 +141,7 @@ upsample_suma_surface.sh \
     $linDepth \
     $hemisphere
 
-## Convert segmentated tissue from surface to volume maks
+echo "++ Convert segmented tissue from surface to volume maks"
 
 suma_dir=$lfmri_mesh_dir/${subj}/SUMA
 upsampled_anat=$lfmri_mesh_dir/${subj}/${subj}_${ses}_res-r0p25_UNIT1_MPRAGEised_biascorrected_fromFS.nii.gz
@@ -171,7 +172,7 @@ convert_afni_surface_to_volume_tissue_mask.sh \
     $linDepth \
     $hemisphere
 
-## Make the RIM to be inspected and manually edited if necessary
+echo "++ Make the RIM to be inspected and manually edited if necessary"
 #  the outout is rim012.nii.gz to be visually inspected and manually edited 
 #  if necessary
 #  You might consider to first align it to EPI space and then manually edit it
@@ -190,7 +191,7 @@ make_afni_GM_WM_rim.sh \
 #      VISUAL INSPECTION AND MANUAL EDITING IF NECESSARY       #
 ################################################################
 
-## Move images to  EPI distorted space
+echo "++ Move images to EPI distorted space"
 
 # Move anatomical to EPI space
 
@@ -223,7 +224,7 @@ resample_ants_image_iso_factor.sh \
     $output_filename \
     $resample_iso_factor
 
-# Move mask/rim to EPI space
+echo "++ Move mask/rim to EPI space"
 # Move rim012 to epi space for manual editing and then reacting final rim and layers
 
 mask_to_warp=$lfmri_layers_dir/${subj}/rim012.nii.gz
@@ -245,7 +246,7 @@ coreg_ants_mask_to_epi.sh \
 #      VISUAL INSPECTION AND MANUAL EDITING IF NECESSARY       #
 ################################################################
 
-## Make final RIM with 1 2 3 labels 
+echo "++ Make final RIM with 1 2 3 labels"
 
 input_rim=$lfmri_layers_dir/${subj}/rim012_space-EPI.nii.gz
 output_dir=$lfmri_layers_dir/${subj}
@@ -263,7 +264,7 @@ make_afni_laynii_rim123.sh \
 #      VISUAL INSPECTION AND MANUAL EDITING IF NECESSARY       #
 ################################################################
 
-## Make layers
+echo "++ Make layers"
 
 mask_roi="rightMT"
 desc="7layers"
