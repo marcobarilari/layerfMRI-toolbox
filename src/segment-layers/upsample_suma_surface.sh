2ef446a6596407c2a36bc6c0868f6cf6d7012053
mrneont <neon.taylor@gmail.com>
2024-02-28 17:29:17 -0500

    upsample_suma_surface.sh: spacing/formatting

--- src/segment-layers/upsample_suma_surface.sh
+++ src/segment-layers/upsample_suma_surface.sh
@@ -4,7 +4,8 @@ set -e
 
 ## Upsample the surface ouptut
 #
-# usage: upsample_SUMA_surface.sh <SUMA_dir> <upsampled_anat> <subID> <linDepth> <hemispher>
+# usage: upsample_SUMA_surface.sh <SUMA_dir> <upsampled_anat> <subID> \
+#                                 <linDepth> <hemisphere>
 
 print_process_logfile.sh \
     $(basename "$0" .sh)
@@ -23,39 +24,37 @@ echo "Moving to $suma_dir"
 echo "Upsampling the surface ouptut for sub-$subID - $hemisphere hemisphere"
 echo ""
 
-MapIcosahedron \
-    -spec sub-${subID}_${hemisphere}.spec \
-    -ld $linDepth \
-    -prefix std_${hemisphere}.ld$linDepth. \
-    -overwrite \
+MapIcosahedron                                                               \
+    -spec    sub-${subID}_${hemisphere}.spec                                 \
+    -ld      $linDepth                                                       \
+    -prefix  std_${hemisphere}.ld$linDepth.                                  \
+    -overwrite                                                               \
     -verb
 
 echo  "++ Dense mesh starting"
 
 # get spec for the new file
-quickspec \
-    -tn gii \
+quickspec                                                                    \
+    -tn  gii                                                                 \
     std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.gii
 
-\mv \
-    quick.spec \
+\mv                                                                          \
+    quick.spec                                                               \
     std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.spec
 
-quickspec \
-    -tn gii \
+quickspec                                                                    \
+    -tn  gii                                                                 \
     std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.gii
 
-\mv \
-    quick.spec \
+\mv                                                                          \
+    quick.spec                                                               \
     std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.spec
 
-inspec \
-    -LRmerge \
-    std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.spec \
-    std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.spec \
-    -detail 2 \
-    -prefix std_BOTH.ld${linDepth}.${hemisphere}.spec \
-    -overwrite
+inspec                                                                       \
+    -LRmerge  std_${hemisphere}.ld${linDepth}.${hemisphere}.smoothwm.spec    \
+              std_${hemisphere}.ld${linDepth}.${hemisphere}.pial.spec        \
+    -detail   2                                                              \
+    -prefix   std_BOTH.ld${linDepth}.${hemisphere}.spec -overwrite
 
 # Go back to previous working directory
 echo ""
